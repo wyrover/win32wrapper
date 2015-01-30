bad3c228725db19ce5ce1bce9ed12b231963cf27
Taben <tabenjmalik@gmail.com>
2015-01-29 17:43:59 -0700

    Added critical section struct

---
+++ WinAPI/CRITICAL_SECTION.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Number One
+ * Date: 1/19/2015
+ * Time: 3:03 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace WinAPI
+{
+	[StructLayout(LayoutKind.Sequential)]
+	struct CRITICAL_SECTION
+	{
+		long LockCount;
+		long RecursionCount;
+	}
+}
---
+++ WinAPI/ProcessorFeature.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Number One
+ * Date: 1/19/2015
+ * Time: 2:42 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace WinAPI
+{
+	
+	public enum ProcessorFeature : uint
+	{
+		FloatingPointPrecisionErrata = 0,
+		FloatingPointEmulated = 1,
+		CompareExchangeDouble = 2,
+		InstructionsMMXAvailable = 3,
+		InstructionsXMMIAvailable = 6,
+		Instruction3DNowAvailable = 7,
+		InstructionRDTSCAvailable = 8,
+		PAEEnabled = 9,
+		InstructionsXMMI64Available = 10,
+		NXEnabled = 12,
+		InstructionsSSE3Available = 13,
+		CompareExchange128 = 14,
+		Compare64Exchange128 = 15,
+		ChannelsEnabled = 16,
+	}
+}
